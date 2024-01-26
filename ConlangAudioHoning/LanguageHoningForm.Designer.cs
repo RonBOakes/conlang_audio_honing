44bc499af5e89f8452a15a050040431a000ce61b
Ron Oakes <ron@ron-oakes.us>
2024-01-26 15:02:51 -0700

    Add the main form.  At this point it does nothing but load and save the language, and exit - and this has not been tested.

---
+++ ConlangAudioHoning/LanguageHoningForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ConlangAudioHoning
+{
+    partial class LanguageHoningForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            loadToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { loadToolStripMenuItem, saveToolStripMenuItem, toolStripSeparator1, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            loadToolStripMenuItem.Size = new Size(180, 22);
+            loadToolStripMenuItem.Text = "Load";
+            loadToolStripMenuItem.Click += loadToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.S;
+            saveToolStripMenuItem.Size = new Size(180, 22);
+            saveToolStripMenuItem.Text = "Save";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.ShortcutKeys = Keys.Alt | Keys.F4;
+            exitToolStripMenuItem.Size = new Size(180, 22);
+            exitToolStripMenuItem.Text = "Exit";
+            // 
+            // LanguageHoningForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "LanguageHoningForm";
+            Text = "LanguageHoningForm";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem loadToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
---
+++ ConlangAudioHoning/LanguageHoningForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConlangJson;
+using LanguageEditor;
+
+namespace ConlangAudioHoning
+{
+    public partial class LanguageHoningForm : Form
+    {
+        private LanguageDescription? languageDescription;
+        private FileInfo? languageFileInfo = null;
+
+        public LanguageHoningForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new())
+            {
+                if (languageFileInfo == null)
+                {
+                    openFileDialog.InitialDirectory = Environment.SpecialFolder.MyDocuments.ToString();
+                }
+                else
+                {
+                    openFileDialog.InitialDirectory = languageFileInfo.DirectoryName;
+                }
+                openFileDialog.Filter = "JSON file (*.json)|*.json|txt file (*.txt)|*.txt|All Files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.Multiselect = false;
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.CheckFileExists = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    LoadLanguage(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new())
+            {
+                if (languageFileInfo == null)
+                {
+                    saveFileDialog.InitialDirectory = Environment.SpecialFolder.MyDocuments.ToString();
+                }
+                else
+                {
+                    saveFileDialog.InitialDirectory = languageFileInfo.DirectoryName;
+                }
+                saveFileDialog.Filter = "JSON file (*.json)|*.json|txt file (*.txt)|*.txt|All Files (*.*)|*.*";
+                saveFileDialog.FilterIndex = 1;
+                saveFileDialog.RestoreDirectory = true;
+                saveFileDialog.CheckFileExists = false;
+                saveFileDialog.OverwritePrompt = true;
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    SaveLanguage(saveFileDialog.FileName);
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void LoadLanguage(string filename)
+        {
+            string jsonString = File.ReadAllText(filename);
+
+            languageDescription = JsonSerializer.Deserialize<LanguageDescription>(jsonString);
+
+            if (languageDescription == null)
+            {
+                MessageBox.Show("Unable to decode Language file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            // TODO: Populate form
+        }
+
+        private void SaveLanguage(string filename)
+        {
+            if (languageDescription == null)
+            {
+                MessageBox.Show("Unable to save language File, no language information present", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                // TODO: Pull the form data into the structure to allow for saving.
+
+                DateTime now = DateTime.UtcNow;
+                string timestamp = now.ToString("o");
+                string history = "Edited in LanguageEditor, saved at " + timestamp;
+
+                if (!languageDescription.metadata.ContainsKey("history"))
+                {
+                    languageDescription.metadata["history"] = new JsonArray();
+                }
+#pragma warning disable CS8600
+                JsonArray historyEntries = (JsonArray)languageDescription.metadata["history"] ?? [];
+#pragma warning restore CS8600
+                historyEntries.Add(history);
+
+                JsonSerializerOptions jsonSerializerOptions = new();
+                jsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+                JavaScriptEncoder encoder = JavaScriptEncoder.Create(UnicodeRanges.All);
+                jsonSerializerOptions.Encoder = encoder;
+                jsonSerializerOptions.WriteIndented = true; // TODO: make an option
+
+                string jsonString = JsonSerializer.Serialize<LanguageDescription>(languageDescription, jsonSerializerOptions);
+                File.WriteAllText(filename, jsonString, System.Text.Encoding.UTF8);
+            }
+        }
+
+    }
+}
