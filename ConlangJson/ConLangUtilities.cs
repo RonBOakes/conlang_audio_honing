6e27dc45d949f3553aca64f68f8c2a65e482e765
Ron Oakes <ron@ron-oakes.us>
2024-02-11 12:17:29 -0700

    Add .NET style header comments to the public methods and properties for all of the classes in ConlangJson.

--- ConlangJson/ConLangUtilities.cs
+++ ConlangJson/ConLangUtilities.cs
@@ -31,8 +31,20 @@ using System.Text.Json;
 
 namespace ConlangJson
 {
+    /// <summary>
+    /// A collection of static utility methods used with the ConlangJson objects.
+    /// </summary>
     public static class ConLangUtilities
     {
+        /// <summary>
+        /// Spell a word - Convert its phonetic representation into a Romanized or Latinized
+        /// representation.
+        /// </summary>
+        /// <param name="phonetic">Phonetic representation of a word using IPA.</param>
+        /// <param name="soundMapList">The SoundMapList (sound_map_list) from the Language.  
+        /// This must be ordered so that processing from the first to the last element will 
+        /// produce the correct spelling.</param>
+        /// <returns>The Romanized or Latinized version of the word.</returns>
         public static string SpellWord(string phonetic, List<SoundMap> soundMapList)
         {
             string spelled = phonetic;
@@ -44,6 +56,15 @@ namespace ConlangJson
             return spelled;
         }
 
+        /// <summary>
+        /// Sound out a word - Convert its Romanized or Latinized form into a phonetic
+        /// form in IPA.
+        /// </summary>
+        /// <param name="word">The Romanized or Latinized version of the word.</param>
+        /// <param name="soundMapList">The SoundMapList (sound_map_list) from the Language.  
+        /// This must be ordered so that processing from the last to the first element will 
+        /// produce the correct phonetic representation.</param>
+        /// <returns>The phonetic representation of the word in IPA.</returns>
         public static string SoundOutWord(string word, List<SoundMap> soundMapList)
         {
             string phonetic = word;
@@ -55,10 +76,22 @@ namespace ConlangJson
             return phonetic;
         }
 
-        /**
-         * Decline a word
-         */
-        public static List<LexiconEntry> DeclineWord(LexiconEntry word, Dictionary<string, List<Dictionary<string, List<Dictionary<string, Affix>>>>> affixMap, List<SoundMap> soundMapList, bool derivedWord = false)
+        /// <summary>
+        /// Decline a single word
+        /// </summary>
+        /// <param name="word">LexiconEntry for the word to be declined</param>
+        /// <param name="affixMap">AffixMap (affix_map) from the language containing the word 
+        /// to be declined</param>
+        /// <param name="soundMapList">The SoundMapList (sound_map_list) from the Language.  
+        /// This must be ordered so that processing from the first to the last element will 
+        /// produce the correct spelling, and processing in the reverse order will produce
+        /// the correct pronunciation.</param>
+        /// <param name="derivedWord">Optional.  Set to true to indicate that the word being
+        /// declined is a derived word.</param>
+        /// <returns>A List of LexiconEntry objects containing the new words created by 
+        /// declining the word parameter according to the supplied AffixMap's rules.</returns>
+        public static List<LexiconEntry> DeclineWord(LexiconEntry word, Dictionary<string, List<Dictionary<string, List<Dictionary<string, Affix>>>>> affixMap, 
+            List<SoundMap> soundMapList, bool derivedWord = false)
         {
             // Safety check - never decline a word already marked as declined, or a word with
             // a source metadata entry
@@ -131,6 +164,10 @@ namespace ConlangJson
             return lexiconFragment;
         }
 
+        /// <summary>
+        /// Decline the complete Lexicon of the supplied language.
+        /// </summary>
+        /// <param name="language">Language to be declined.</param>
         public static void declineLexicon(LanguageDescription language)
         {
             List<LexiconEntry> addLexicon = new List<LexiconEntry>();
@@ -140,14 +177,14 @@ namespace ConlangJson
             }
             language.lexicon.AddRange(addLexicon);
             List<LexiconEntry> cleanLexicon = deDuplicateLexicon(language.lexicon);
-            if(cleanLexicon.Count <  language.lexicon.Count)
-            {
-                language.lexicon = cleanLexicon;
-            }
-            language.lexicon.Sort(new LexiconEntry.LexicalOrderCompSpelling());
             language.declined = true;
         }
 
+        /// <summary>
+        /// Remove all of the entries in the supplied language's lexicon that were created
+        /// as the result of a programmatic declension of the language.  
+        /// </summary>
+        /// <param name="language">Language to have its declined words removed from the lexicon</param>
         public static void removeDeclinedEntries(LanguageDescription language) 
         {
             List<LexiconEntry> cleanLexicon = new List<LexiconEntry>();
@@ -167,6 +204,11 @@ namespace ConlangJson
             language.declined = false;
         }
 
+        /// <summary>
+        /// Remove any duplicate entries in the supplied Lexicon.<br/>This method is slow.
+        /// </summary>
+        /// <param name="lexicon">List of LexiconEntry objects.</param>
+        /// <returns>A list of LexiconEntry objects with any duplicates removed.</returns>
         public static List<LexiconEntry> deDuplicateLexicon(List<LexiconEntry> lexicon)
         {
             List<LexiconEntry> newLexicon = new List<LexiconEntry>();
@@ -459,7 +501,7 @@ namespace ConlangJson
             {
                 return false;
             }
-            // Asumption: neither list will have duplicate entries - safe for our needs here.
+            // Assumption: neither list will have duplicate entries - safe for our needs here.
             int matchCount = 0;
             foreach (T t in one)
             {
