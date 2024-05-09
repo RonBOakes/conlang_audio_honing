0e8cbd2bd629206e132b3c64a809f4ac5fb0b12e
Ron Oakes <ron@ron-oakes.us>
2024-05-09 12:04:49 -0600

    Update code to add missing GPL 3.x headers.
    NOTE: the .resx files are missing these headers because the code will
    not build if they are included in those files.

--- PolyGlot2ConlangJson/PolyGlot2ConlangJsonMain.cs
+++ PolyGlot2ConlangJson/PolyGlot2ConlangJsonMain.cs
@@ -1,4 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*
+ * Main program for the tool to convert PolyGlot save files into the Conlang JSON object format
+ * Copyright (C) 2024 Ronald B. Oakes
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// See https://aka.ms/new-console-template for more information
 
 using ConlangJson;
 using Mono.Options;
